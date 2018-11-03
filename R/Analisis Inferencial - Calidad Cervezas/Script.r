d301401ecff355fcdab26a445d1a64938b5ffe93
luisorellana777 <luis.orellana777@gmail.com>
2018-11-03 03:43:02 -0300

    Add files via upload
    R Scripts

---
+++ R/Analisis Inferencial - Calidad Cervezas/Script.r
@@ -0,0 +1,54 @@
+#Ruta del Dataset
+RUTA_DATASET <- "C:/Users/Luis.O.A/Documents/Analisis Cervezas"
+setwd(RUTA_DATASET)
+
+#Leer Dataset
+data=read.csv("beer_reviews.csv")
+
+#Eliminar datos NA
+data= na.omit(data)
+
+#Resumen medidas de tendencia 
+summary(data)
+
+#Desviacion estandar para observar desviacion con respecto a la media
+sd(data$review_appearance)
+sd(data$review_aroma)
+sd(data$review_palate)
+sd(data$review_taste)
+
+
+#¿Qué cervecería produce la cerveza más fuerte según ABV? 
+unique(data$brewery_name[data$beer_abv == max(data$beer_abv)])
+
+#¿Si tuviera que elegir 3 cervezas para recomendar usando sólo estos datos, cuáles elegiría?
+#El siguente sumatoria de los campos "review" es equivalente al campo "review_overall"
+new_data <- data.frame(nom = data$beer_name, suma = data$review_aroma + data$review_appearance + data$review_palate + data$review_taste)
+grupo <- aggregate(new_data$suma, by=list(nom=new_data$nom), FUN=sum)
+
+head(grupo[order(grupo$x, decreasing = TRUE),], 3)
+
+#¿Cual de los factores (aroma, taste, aperance, palete) es ams importante para 
+#determinar la calidad general de una cerveza?
+
+hist(data$review_aroma, xlab="review_aroma", main="review_aroma")
+hist(data$review_taste, xlab="review_taste", main="review_taste")
+hist(data$review_appearance, xlab="review_appearance", main="review_appearance")
+hist(data$review_palate, xlab="review_palate", main="review_palate")
+
+hist(data$review_overall, xlab="review_overall", main="review_overall")
+
+d <- data.frame(data$review_aroma, 
+data$review_taste, 
+data$review_appearance, 
+data$review_palate)
+
+cor(d, data$review_overall, method = "pearson")
+
+#Si yo tipicamente disfruto una cerveza debido a su 
+#aroma y apariencia, ¿que estilo de cerveza deberia probar?
+new_ar_ap <- data.frame(nom = data$beer_style, suma = data$review_aroma + data$review_appearance)
+    
+grupo <- aggregate(new_ar_ap$suma, by=list(nom=new_ar_ap$nom), FUN=sum)
+head(grupo[order(grupo$x, decreasing = TRUE),], 1)
+  
---
+++ R/Arboles de Decision - Zoo/Arboles de Decision.r
@@ -0,0 +1,48 @@
+#ARBOL BINARIO
+#Carga dataset ZOO
+data_zoo  =  read.csv("zoo-tree1.csv") 
+#Discretiza datos
+data_zoo["animal_name"] <- NULL
+data_zoo <- data.frame(sapply(data_zoo,as.factor)) 
+# Clasifica los modelos de árbol usando el algoritmo de Quinlan C5.0 
+c50_result<-C5.0(class_type~.,data=data_zoo) 
+# Despliega resumen detallado para los modelos C.50 
+summary(c50_result) 
+# calcula y despliega la importancia de la variable para el modelo C5.0
+ importanceC5imp(c50_result,metric='usage')
+
+#REGLAS.
+
+# Generar reglas a partir del arcol de decisión de los modelos C.50 
+ruleModel <- C5.0(type ~ ., data = data_zoo, rules = TRUE)
+ruleModel
+# Despliega resumen detallado para las reglas de los modelos C.50
+summary(ruleModel)
+
+#VALIDACION CRUZADA.
+library(arulesViz)
+install.packages('partykit', dep = TRUE)
+install.packages('gmodels', dep = TRUE)
+library(partykit)
+library(C50)
+library(gmodels)
+ library(plyr)
+data_zoo  =  read.csv("zoo-tree1.csv") #Carga dataset
+data_zoo["animal_name"] <- NULL
+data_zoo <- data.frame(sapply(data_zoo,as.factor)) #Discretiza datos
+
+ form <- "class_type  ~ hair+feathers+eggs+milk+airborne+aquatic+predator+toothed+backbone+breathes+venomous+fins+legs0+legs2+legs4+legs5+legs6+legs8+tail+domestic+catsize" 
+
+folds <- split(data_zoo , cut(sample(1:nrow(data_zoo)),10)) 
+length(folds)
+folds
+errs.c50 <- rep(NA, length(folds))
+for (i in 1:length(folds)) { 
+test <- ldply(folds[i], data.frame) 
+train <- ldply(folds[-i], data.frame) 
+tmp.model <- C5.0(as.formula(form), train) 
+tmp.predict <- predict(tmp.model, newdata=test) 
+conf.mat <- table(test$class_type , tmp.predict) 
+errs.c50[i] <- 1 - sum(diag(conf.mat))/sum(conf.mat)
+ }
+print(sprintf("El promedio de error usando la validación cruzada k-fold y el algoritmo de arbol de decision  C5.0 es: %.3f percent", 100*mean(errs.c50))) 
