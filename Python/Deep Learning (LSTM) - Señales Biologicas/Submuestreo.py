5b0126aaf639394e6190dff8eacaa989112f9812
Luis Orellana <luis.orellana777@gmail.com>
2019-01-01 17:20:52 -0300

    Resampling stairs of MAP

--- Python/Deep Learning (LSTM) - Señales Biologicas/Submuestreo.py
+++ Python/Deep Learning (LSTM) - Señales Biologicas/Submuestreo.py
@@ -14,32 +14,66 @@ from sklearn import preprocessing
 from matplotlib import pyplot as plt
 from scipy import signal
 
-PATH = "C:/Users/Luis.O.A/Documents/USACH/Tesis/Dataset/Sujetos"
+class Signal:
+    PATH = "C:/Users/Luis.O.A/Documents/USACH/Tesis/Dataset/Sujetos"
+    
+    def __init__(self, filePath):
+        self.filePath = filePath
+        
+    def reesampling(self, var):
+        x_resampled = signal.resample(var, math.ceil(var.shape[0]*1.33333333333333))
+        return x_resampled
+     
+    def process(self):
+        X = pd.read_csv(self.filePath, sep="	")
+        
+        VFSCd = self.reesampling(X['VFSCd'])
+        VFSCi = self.reesampling(X['VFSCi'])
+        PAMn = self.reesampling(X['PAMn'])
+        
+        
+        reesampled_data = zip(VFSCd,VFSCi, PAMn)
+        
+        with open(self.filePath[:(len(self.filePath)-3)] + "csv", 'w', newline='') as f:
+            writer = csv.writer(f, delimiter='\t')
+            writer.writerow(["VFSCd", "VFSCi", "PAMn"])
+            writer.writerows(reesampled_data)
+        
+def run_signal():    
+    listOfFolders = os.listdir(Signal.PATH)
+    for folder in listOfFolders:
+        folderPath = Signal.PATH+"/"+folder
+        listOfFiles = os.listdir(folderPath)
+        for file in listOfFiles:
+            filePath = folderPath+"/"+file
+            signal = Signal(filePath)
+            signal.process()
 
-def reesampling(var):
-    x_resampled = signal.resample(var, math.ceil(var.shape[0]*1.33333333333333))
-    return x_resampled
- 
-def process(filePath):
-    X = pd.read_csv(filePath, sep="	")
-    
-    VFSCd = reesampling(X['VFSCd'])
-    VFSCi = reesampling(X['VFSCi'])
-    PAMn = reesampling(X['PAMn'])
-    
-    
-    reesampled_data = zip(VFSCd,VFSCi, PAMn)
-    
-    with open(filePath[:(len(filePath)-3)] + "csv", 'w', newline='') as f:
-        writer = csv.writer(f, delimiter='\t')
-        writer.writerow(["VFSCd", "VFSCi", "PAMn"])
-        writer.writerows(reesampled_data)
-    
-    
-listOfFolders = os.listdir(PATH)
-for folder in listOfFolders:
-    folderPath = PATH+"/"+folder
-    listOfFiles = os.listdir(folderPath)
-    for file in listOfFiles:
-        filePath = folderPath+"/"+file
-        process(filePath)
+
+class Stair:
+        
+    def __init__(self, file_stair):
+        self.file_stair = file_stair
+
+    def reesampling(self, var):
+        x_resampled = signal.resample(var, math.ceil(var.shape[0]*0.52977777645333333333333333333333))
+        return x_resampled
+     
+    def process(self):
+        X = pd.read_csv(self.file_stair, sep="	")
+        
+        escalon = self.reesampling(X['ESCALON'])
+
+        reesampled_data = zip(escalon)
+        
+        with open(self.file_stair[:(len(self.file_stair)-4)] + "_04.csv", 'w', newline='') as f:
+            writer = csv.writer(f, delimiter='\t')
+            writer.writerow(["ESCALON"])
+            writer.writerows(reesampled_data)
+
+def run_stair():   
+    file_stair = "C:/Users/Luis.O.A/Documents/USACH/Tesis/Dataset/esc.csv"     
+    stair = Stair(file_stair)
+    stair.process()
+
+run_stair()
