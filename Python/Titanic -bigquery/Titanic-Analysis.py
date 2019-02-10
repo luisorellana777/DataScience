539519447783c81d80667c5cd02349abd9f30035
Luis Orellana <luis.orellana777@gmail.com>
2019-02-10 20:21:22 -0300

    add new analysis through random forest

--- Python/Titanic -bigquery/Titanic-Analysis.py
+++ Python/Titanic -bigquery/Titanic-Analysis.py
@@ -1,5 +1,4 @@
 import pandas as pd
-# to communicate with Google BigQuery
 from pandas.io import gbq
 import plotly.plotly as py
 import plotly.graph_objs as go
@@ -10,7 +9,7 @@ project_id = 'titanic-231219'
 top10_active_users_query = "SELECT * FROM [titanic-231219.titanic.titanic_passenger] LIMIT 10"
 
 
-top10_active_users_df = gbq.read_gbq(top10_active_users_query, project_id=project_id)
+top10_active_users_df = gbq.to_gbq(top10_active_users_query, project_id=project_id)
 
 print (top10_active_users_df)
 
